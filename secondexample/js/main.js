6a4c3b07f0eded8b3051c94bd035211734b07447
WayneMorrissey <38153810+WayneMorrissey@users.noreply.github.com>
2018-09-10 09:35:40 -0600

    added other project

---
+++ secondexample/js/main.js
@@ -0,0 +1,26 @@
+// console.log()
+
+/* 
+EMCAScript
+- standard
+- ES5 spec
+- ES6
+- ES2017
+- ES2018
+
+Rules ECM
+- dot syntax
+statment js
+obj.prop/method.value
+
+Property Statement
+obj.prop = value;
+navigatior.name //read only
+window.name = "new window";  Read/Write or Read only
+
+method statement
+obj.method(arg);
+console.log('message')
+*/
+
+console.log("finally some js");
