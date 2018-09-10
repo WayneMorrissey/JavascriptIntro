65675b47e65d03a4a8d19b275a21a55778b15b18
WayneMorrissey <38153810+WayneMorrissey@users.noreply.github.com>
2018-09-10 09:50:52 -0600

    again

--- secondexample/js/main.js
+++ secondexample/js/main.js
@@ -21,6 +21,11 @@ window.name = "new window";  Read/Write or Read only
 method statement
 obj.method(arg);
 console.log('message')
+
+Variables 25
+var name assignment operator value
+var logidID = 123;
 */
 
+var userID = 123;
 console.log("finally some js");
