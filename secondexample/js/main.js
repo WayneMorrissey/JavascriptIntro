b1b6d3bc308a118bd891d286e27f780f7e84bd06
WayneMorrissey <38153810+WayneMorrissey@users.noreply.github.com>
2018-09-12 09:42:39 -0600

    selectors

--- secondexample/js/main.js
+++ secondexample/js/main.js
@@ -27,5 +27,19 @@ var name assignment operator value
 var logidID = 123;
 */
 
-var userID = 123;
+/* var userID = 123;
 console.log("finally some js");
+
+ */
+
+ /* 
+    Let
+    Const
+ */
+
+ var cogListItem;
+ cogListItem = document.querySelector('.icon-view li:last-child');
+ console.log(cogListItem);
+ var userName;
+ userName = "username";
+ console.log(userName);
