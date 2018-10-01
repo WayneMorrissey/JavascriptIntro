ea2811f473c16de04d2b5cd317757d3cccadf5b1
WayneMorrissey <38153810+WayneMorrissey@users.noreply.github.com>
2018-10-01 09:37:43 -0600

    added data set project

---
+++ data-set/js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(loadEvent){
+    var dataArray = [];
+    // use a loop to build our objects
+    var cardObj=
+    {
+        id:1,
+        src: "img/strain-1.jpg"
+    };
+
+    dataArray.push(cardObj);
+    
+    var objectsView = document.querySelector(".card");
+    objectsView.addEventListener("click", function(e){
+        console.log(e.currentTarget.dataset.index);
+    })
+
+})
