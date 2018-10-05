720f4fe4674d01da3ae15b3d93777b2146a04a73
Wayne <38153810+Wayne@users.noreply.github.com>
2018-10-05 14:50:34 -0600

    fixed, working, might need more validation

--- editing_gallery/js/main.js
+++ editing_gallery/js/main.js
@@ -13,15 +13,18 @@ window.addEventListener('load', function(){
     var error = '';
     var selectedImg;
 
+    // error messages
     var nullError = "A tag cannot be empty";
     var spacesError = "A tag cannot include spaces";
     var dupeError = "That tag already exists";
 
 
+    // cycles through thumbs array to create a matching obj instance and event listeners
     for(var i = 0; i < thumbs.length; i++)
     {
         imgObjs[i] =
         {
+            // the thumbnail image src is slightly different than full image
             src: thumbs[i].src.replace('_thumb', ''),
             alt: thumbs[i].alt,
             title: thumbs[i].title,
@@ -29,13 +32,17 @@ window.addEventListener('load', function(){
         }
         thumbs[i].addEventListener('click', onThumbnailClicked);   
     }
+    // sets the default selected img to the first in the array
     selectedImg = imgObjs[0];
+    // add event listener for the entire form
     form.addEventListener('submit', onFormSubmit);
 
+    // handles the selection of a thumbnail image
     function onThumbnailClicked(e)
     {
-        console.log(e.currentTarget);
+        // index to represent where in the array the img selected is
         var index;
+        // sets the index to the array index of the image clicked on
         switch(e.currentTarget)
         {
             case thumbs[0]:
@@ -48,33 +55,48 @@ window.addEventListener('load', function(){
                 index = 2;
                 break;
         }
+        // change the html element for the editor image to match selected img
         editImg.src = imgObjs[index].src;
         editImg.alt = imgObjs[index].alt;
         editImg.title = imgObjs[index].title;
+        // set html fields for tags and title to match selected img
         tags.innerHTML = imgObjs[index].tags;
         title.innerHTML = imgObjs[index].title;
+        // save the selected img obj instance to reference for tag entry
         selectedImg = imgObjs[index];
+        // reset error message to null then refresh ui to clear the html message
+        error = '';
+        refreshUI();
     }
 
-
+    // Handles the submission of the form
     function onFormSubmit(e)
     {
         e.preventDefault();
         var tag = tagInput.value;
+        //trim spaces from front and back of input string
         tag = tag.trim();
+        //check if user added their own hashtag, if so, remove it for validation
+        if(tag.charAt(0) === '#')
+        {
+            tag = tag.slice(1);
+        }
+        // throw error if input is null
         if(tag === "")
         {
             error = nullError;
         }
+        // throw error if input includes a space 
         else if (tag.includes(" "))
         {
             error = spacesError;
         }
-        
-        else if(selectedImg.tags.toString().splt(" ").includes('#' + tag))
+        // split the tags data string into an array, then check if an identical tag exist
+        else if(selectedImg.tags.split(" ").includes('#' + tag))
         {
             error = dupeError;
         }
+        // if tag passes error checking, add the tag with a # to our image
         else
         {
             selectedImg.tags += ' #' + tag;
@@ -83,11 +105,15 @@ window.addEventListener('load', function(){
         refreshUI();
     }
 
+    // Refreshes the error messages to either hide or show error info
+    // also sets the tags text to the currently selected image
     function refreshUI()
     {
         if(error === '')
         {
             errorElem.classList.add('hidden');
+            // clear tag value since either the tag was added or the image was changed
+            tagInput.value = "";
         }
         else
         {
