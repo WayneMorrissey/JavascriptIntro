60c6c0ab2d03e3cf07b838cb50e2d83d8b4b2a3b
WayneMorrissey <38153810+WayneMorrissey@users.noreply.github.com>
2018-10-26 09:27:24 -0600

    added dom project and array in class

---
+++ dom/js/main.js
@@ -0,0 +1,17 @@
+/* 
+ON SUBMIT
+    -Data Object
+*/
+
+window.addEventListener("load", function()
+{
+    let createdItems = [];
+    const inputForm = document.querySelector(".data-entry");
+
+    inputForm.addEventListener("submit", function(e)
+    {
+        e.preventDefault();
+        const inputItems = (e.currentTarget.elements);
+        const dataObj =
+    })
+})
